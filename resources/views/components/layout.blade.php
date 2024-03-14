6e479220f81f6e73a45666649113a45139adaecd
Saullo Almeida <saullorhamon@gmail.com>
2024-03-13 21:34:53 -0300

    add nav-link component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,10 +8,11 @@
 </head>
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
     <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <x-nav-link href="/" label="Home" />
+        <x-nav-link href="/about" label="About" />
+        <x-nav-link href="/contact" label="Contact" />
     </nav>
+
     {{ $slot }}
 </body>
 </html>
