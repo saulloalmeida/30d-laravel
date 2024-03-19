c7f7baab35dd7fd9a03d0e576a7c2cd512ee3987
Saullo Almeida <saullorhamon@gmail.com>
2024-03-18 22:33:54 -0300

    Revert "Revert "add type prop into a nav-link component""
    This reverts commit bc1652bd70cacab58d29261d552c1200bd9a46e8.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,16 @@
-<a href="{{ $attributes }}">{{ $slot }}</a>
+@props(["active" => false, "typeNav" => "a"])
+@if($typeNav == "a")
+    <a
+        class="{{$active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium"
+        aria-current="{{$active ? 'page': 'false'}}"
+        {{ $attributes }}
+    >{{$slot}}</a>
+@endif
+
+@if($typeNav == "button")
+    <button
+        class="{{$active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium"
+        aria-current="{{$active ? 'page': 'false'}}"
+        {{ $attributes }}
+    >{{$slot}}</button>
+@endif
