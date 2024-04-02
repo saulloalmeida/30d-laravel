4826941813f4af369750caa8248c12223a7b06e6
Saullo Almeida <saullorhamon@gmail.com>
2024-04-01 22:48:24 -0300

    create JobListings migration

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,32 +1,44 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Support\Arr;
 
-class Job {
+class Job
+{
     public static function all(): array
     {
         return [
             [
-                'id' => 1,
-                'title' => 'Director of Engineering',
-                'salary' => 100,000,
+                'id'     => 1,
+                'title'  => 'Director of Engineering',
+                'salary' => 100,
+                0,
             ],
             [
-                'id' => 2,
-                'title' => 'Director of Marketing',
-                'salary' => 80,000
+                'id'     => 2,
+                'title'  => 'Director of Marketing',
+                'salary' => 80,
+                0
             ],
             [
-                'id' => 3,
-                'title' => 'Director of Sales',
-                'salary' => 70,000
+                'id'     => 3,
+                'title'  => 'Director of Sales',
+                'salary' => 70,
+                0
             ],
         ];
     }
+
     public static function find(int $id): array
     {
-        return Arr::first(static::all(), fn($job) => $job['id'] == $id);
+        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+        if (!$job) {
+            abort(404);
+        }
+
+        return $job;
     }
 }
